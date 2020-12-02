e43d5743ce2ab5f5ed282d36742baf88fa2837fd
anna-ip <anna.almestal@gmail.com>
2020-12-02 09:51:17 +0100

    added the sounds to the buttons

--- game.js
+++ game.js
@@ -2,16 +2,36 @@ const userClickedPattern = []
 const buttonColours = ['red', 'blue', 'green', 'yellow']
 const gamePattern = []
 
-const nextSequence = () => {
-  // the btn clicked id is stored in userChosenColor
-  $('div.btn').click((event) => {
-    const userChosenColour = $(event.target).attr('id')
-    console.log(userChosenColour)
-    userClickedPattern.push(userChosenColour)
-    console.log(userClickedPattern)
-  })
+// the btn clicked id is stored in userChosenColor
+// $('div.btn').click((event) => {
+//   const userChosenColour = $(event.target).attr('id')
+//   console.log(userChosenColour)
+//   userClickedPattern.push(userChosenColour)
+//   console.log(userClickedPattern)
+// })
 
-  let randomNumber = Math.floor(Math.random() * 4)
+// $('div.btn').click((event) => {
+//   const userChosenColour = $(event.target).attr('id')
+//   console.log(userChosenColour)
+//   userClickedPattern.push(userChosenColour)
+//   console.log(userClickedPattern)
+// })
+
+const playSound = (name) => {
+  $('div.btn').click((event) => {
+    const name = $(event.target).attr('id')
+    console.log(name)
+    userClickedPattern.push(name)
+    console.log(userClickedPattern)
+
+    var audio = new Audio('sounds/' + name + '.mp3')
+    audio.play()
+  })
+}
+playSound()
+
+const nextSequence = () => {
+  const randomNumber = Math.floor(Math.random() * 4)
   //console.log(randomNumber)
   const randomChosenColour = buttonColours[randomNumber]
   gamePattern.push(randomChosenColour)
@@ -23,7 +43,6 @@ const nextSequence = () => {
     .fadeOut(100)
     .fadeIn(100)
 
-  //** Adding the sound **/
   var audio = new Audio('sounds/' + randomChosenColour + '.mp3')
   audio.play()
 }
