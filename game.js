f244f7c91f4805b13a1574fec5302b3d4cd974c6
anna-ip <anna.almestal@gmail.com>
2020-12-03 14:39:24 +0100

    finished the simon game, added the logic when you get game over and a startover function.

--- game.js
+++ game.js
@@ -1,16 +1,71 @@
 const buttonColours = ['red', 'blue', 'green', 'yellow']
-const gamePattern = []
-const userClickedPattern = []
 
-$('div.btn').click((event) => {
-  const userChosenColour = $(event.target).attr('id')
-  console.log(userChosenColour)
-  userClickedPattern.push(userChosenColour)
-  console.log(userClickedPattern)
+var gamePattern = []
+let userClickedPattern = []
 
-  playSound(userChosenColour)
-  animatePress(userChosenColour)
+let level = 0
+let started = false
+
+$(document).keypress(() => {
+  if (!started) {
+    $('#level-title').text('Level ' + level)
+    nextSequence()
+    started = true
+  }
 })
+
+$('.btn').click((event) => {
+  const userChosenColour = $(event.target).attr('id')
+  userClickedPattern.push(userChosenColour)
+  console.log('You clicked:', userClickedPattern)
+
+  playSound(userChosenColour) //invokes the playsound function and sending userChosenColor as a parameter
+  animatePress(userChosenColour) //invokes the animatePress function and sending userChosenColor as a parameter
+
+  checkAnswer(userClickedPattern.length - 1)
+})
+
+const checkAnswer = (currentLevel) => {
+  console.log(currentLevel)
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log('Success')
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence()
+      }, 1000)
+    }
+  } else {
+    console.log('Too slow')
+    playSound('wrong')
+
+    $('body').addClass('game-over')
+    setTimeout(() => {
+      $('body').removeClass('game-over')
+    }, 200)
+
+    $('#level-title').text('Game Over, Press Any Key to Restart')
+
+    startOver()
+  }
+}
+
+const nextSequence = () => {
+  userClickedPattern = []
+  level++
+  $('#level-title').text('Level ' + level)
+
+  const randomNumber = Math.floor(Math.random() * 4)
+  const randomChosenColour = buttonColours[randomNumber]
+  gamePattern.push(randomChosenColour)
+
+  //** Flash animation added to clicked btn **/
+  $('#' + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100)
+
+  playSound(randomChosenColour)
+}
 
 const playSound = (name) => {
   var audio = new Audio('sounds/' + name + '.mp3')
@@ -25,19 +80,8 @@ const animatePress = (currentColor) => {
   }, 100)
 }
 
-const nextSequence = () => {
-  const randomNumber = Math.floor(Math.random() * 4)
-  //console.log(randomNumber)
-  const randomChosenColour = buttonColours[randomNumber]
-  gamePattern.push(randomChosenColour)
-  //return randomNumber
-
-  //** Flash animation added to clicked btn **/
-  $('#' + randomChosenColour)
-    .fadeIn(100)
-    .fadeOut(100)
-    .fadeIn(100)
-
-  var audio = new Audio('sounds/' + randomChosenColour + '.mp3')
-  audio.play()
+const startOver = () => {
+  level = 0
+  gamePattern = []
+  started = false
 }
